c721e229a917d7865d4b6d26ec0d09cac99f0f3c
zqktlwi4fecvo6ri <jemmanuel903@gmail.com>
2022-08-26 11:11:57 +0700

    Migration Many to Many

--- database/migrations/2022_08_26_035750_student_course.php
+++ database/migrations/2022_08_26_035750_student_course.php
@@ -15,9 +15,9 @@ class StudentCourse extends Migration
     {
         Schema::create('student_courses', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('student_id');
+            $table->unsignedBigInteger('student_id');
             $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
-            $table->bigInteger('course_id');
+            $table->unsignedBigInteger('course_id');
             $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
         });
     }
