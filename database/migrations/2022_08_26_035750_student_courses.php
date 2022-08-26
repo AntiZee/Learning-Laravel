2035664d7f66b436e6e5709d1d3db7f2764a504c
zqktlwi4fecvo6ri <jemmanuel903@gmail.com>
2022-08-26 12:05:58 +0700

    .

--- database/migrations/2022_08_26_035750_student_courses.php
+++ database/migrations/2022_08_26_035750_student_courses.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class StudentCourse extends Migration
+class StudentCourses extends Migration
 {
     /**
      * Run the migrations.
