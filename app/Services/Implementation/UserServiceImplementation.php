374c17c5e599b4e4a4cdbd81e4d57210e3e1cb22
unknown <jemmanuel903@gmail.com>
2022-09-01 16:41:11 +0700

    TDL Init

---
+++ app/Services/Implementation/UserServiceImplementation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services\Implementation;
+use App\Services\UserService;
+
+class UserServiceImplementation implements UserService
+{
+    private array $users = [
+        'Alfa' => 'Alfa0',
+        'Beta' => 'Beta0',
+        'Gamma' => 'Gamma0'
+    ];
+    function login(string $user, string $pass): bool
+    {
+        if (!isset($this->users[$user])) {
+            return false;
+        }
+        $correctPass = $this->users[$user];
+        return $pass == $correctPass;
+    }
+}
