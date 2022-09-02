f1682a26965fcee4dc481888fdbe6812665b8e56
unknown <jemmanuel903@gmail.com>
2022-09-02 17:45:51 +0700

    Todolist

--- config/app.php
+++ config/app.php
@@ -175,7 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\AdminServiceProvider::class,
-        App\Providers\StudentServiceProvider::class
+        App\Providers\StudentServiceProvider::class,
+        App\Providers\TodolistServiceProvider::class
     ],
 
     /*
