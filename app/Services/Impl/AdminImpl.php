d14341006fee683ef17195ae22504009ff17115f
unknown <jemmanuel903@gmail.com>
2022-09-02 14:42:51 +0700

    Impl

---
+++ app/Services/Impl/AdminImpl.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services\Impl;
+
+use App\Services\AdminService;
+
+class AdminImpl implements AdminService
+{
+}
---
+++ app/Services/Impl/StudentImpl.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services\Impl;
+
+use App\Services\StudentService;
+
+class StudentImpl implements StudentService
+{
+}
